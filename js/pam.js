15940bf606b95c7cf00e8c93587b82ea6ca9fd7e
evox1994 <dmitrybatukhtin@gmail.com>
2017-12-27 22:44:28 +0700

    dfgbdfgb

---
+++ js/pam.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.pam-work').mouseover(function(){
+		$(this).addClass("hover");
+	});
+	$('.pam-work').mouseout(function(){
+		$(this).removeClass("hover");
+	});
+});
