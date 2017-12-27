e1dec7cf80f6fbf229155cc1b58287e57a89f17e
evox1994 <dmitrybatukhtin@gmail.com>
2017-12-28 01:09:48 +0700

    gbdgbdfgb

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	var scroll = $('.b-1-works').offset().top;
 	$('.pam-work').mouseover(function(){
 		$(this).addClass("hover");
 	});
@@ -13,10 +14,19 @@ $(document).ready(function(){
 		}
 		return false;
 	});
+	$('.b-1-buttons a').click(function(){
+		if ( $(this).hasClass("active") ) {
+			$(this).removeClass("active");
+		} else {
+			$(this).addClass("active");
+		}
+		return false;
+	});
 	$('.b-1-works-nav a').click(function(){
 		if ( !($(this).hasClass("active")) ) {
 			$('.b-1-works-nav a').removeClass("active");
 			$(this).addClass("active");
+			$('body, html').animate({ scrollTop: scroll }, 800);
 		}
 		return false;
 	});
